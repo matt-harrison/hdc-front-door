57973179a50ccb53efae888f501fa8aed6e21430
Matt Harrison <matthewdh@gmail.com>
2019-04-21 16:18:05 -0400

    initial development.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,202 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/main.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="mt-1 mx-auto mb-1 container">
+          <div className="w-full">
+            <div className="mb-1 bg-orange color-white text-center">Max content well of 784px</div>
+            <div className="flex mb-1 color-white">
+              <div className="mr-2 w-1/2 bg-orange text-center">1/2</div>
+              <div className="w-1/2 bg-orange text-center">1/2</div>
+            </div>
+            <div className="flex mb-1 color-white">
+              <div className="mr-2 w-1/3 bg-orange text-center">1/3</div>
+              <div className="mr-2 w-1/3 bg-orange text-center">1/3</div>
+              <div className="w-1/3 bg-orange text-center">1/3</div>
+            </div>
+            <div className="flex mb-1 color-white">
+              <div className="mr-2 w-2/3 bg-blue text-center">2/3</div>
+              <div className="w-1/3 bg-orange text-center">1/3</div>
+            </div>
+            <div className="flex mb-1 color-white">
+              <div className="mr-2 w-1/3 bg-orange text-center">1/3</div>
+              <div className="w-2/3 bg-blue text-center">2/3</div>
+            </div>
+            <div className="flex mb-1 color-white">
+              <div className="mr-2 w-1/4 bg-orange text-center">1/4</div>
+              <div className="mr-2 w-1/4 bg-orange text-center">1/4</div>
+              <div className="mr-2 w-1/4 bg-orange text-center">1/4</div>
+              <div className="w-1/4 bg-orange text-center">1/4</div>
+            </div>
+            <div className="flex mb-1 color-white">
+              <div className="mr-2 w-2/4 bg-blue text-center">2/4</div>
+              <div className="mr-2 w-1/4 bg-orange text-center">1/4</div>
+              <div className="w-1/4 bg-orange text-center">1/4</div>
+            </div>
+            <div className="flex mb-1 color-white">
+              <div className="mr-2 w-1/4 bg-orange text-center">1/4</div>
+              <div className="mr-2 w-2/4 bg-blue text-center">2/4</div>
+              <div className="w-1/4 bg-orange text-center">1/4</div>
+            </div>
+            <div className="flex mb-1 color-white">
+              <div className="mr-2 w-1/4 bg-orange text-center">1/4</div>
+              <div className="mr-2 w-1/4 bg-orange text-center">1/4</div>
+              <div className="w-2/4 bg-blue text-center">2/4</div>
+            </div>
+            <div className="flex mb-1 color-white">
+              <div className="mr-2 w-1/5 bg-orange text-center">1/5</div>
+              <div className="mr-2 w-1/5 bg-orange text-center">1/5</div>
+              <div className="mr-2 w-1/5 bg-orange text-center">1/5</div>
+              <div className="mr-2 w-1/5 bg-orange text-center">1/5</div>
+              <div className="w-1/5 bg-orange text-center">1/5</div>
+            </div>
+            <div className="flex mb-1 color-white">
+              <div className="mr-2 w-2/5 bg-blue text-center">2/5</div>
+              <div className="mr-2 w-1/5 bg-orange text-center">1/5</div>
+              <div className="mr-2 w-1/5 bg-orange text-center">1/5</div>
+              <div className="w-1/5 bg-orange text-center">1/5</div>
+            </div>
+            <div className="flex mb-1 color-white">
+              <div className="mr-2 w-1/5 bg-orange text-center">1/5</div>
+              <div className="mr-2 w-2/5 bg-blue text-center">2/5</div>
+              <div className="mr-2 w-1/5 bg-orange text-center">1/5</div>
+              <div className="w-1/5 bg-orange text-center">1/5</div>
+            </div>
+            <div className="flex mb-1 color-white">
+              <div className="mr-2 w-1/5 bg-orange text-center">1/5</div>
+              <div className="mr-2 w-1/5 bg-orange text-center">1/5</div>
+              <div className="mr-2 w-2/5 bg-blue text-center">2/5</div>
+              <div className="w-1/5 bg-orange text-center">1/5</div>
+            </div>
+            <div className="flex mb-1 color-white">
+              <div className="mr-2 w-1/5 bg-orange text-center">1/5</div>
+              <div className="mr-2 w-1/5 bg-orange text-center">1/5</div>
+              <div className="mr-2 w-1/5 bg-orange text-center">1/5</div>
+              <div className="w-2/5 bg-blue text-center">2/5</div>
+            </div>
+            <div className="flex mb-1 color-white">
+              <div className="mr-2 w-3/5 bg-blue text-center">3/5</div>
+              <div className="mr-2 w-1/5 bg-orange text-center">1/5</div>
+              <div className="w-1/5 bg-orange text-center">1/5</div>
+            </div>
+            <div className="flex mb-1 color-white">
+              <div className="mr-2 w-1/5 bg-orange text-center">1/5</div>
+              <div className="mr-2 w-3/5 bg-blue text-center">3/5</div>
+              <div className="w-1/5 bg-orange text-center">1/5</div>
+            </div>
+            <div className="flex mb-1 color-white">
+              <div className="mr-2 w-1/5 bg-orange text-center">1/5</div>
+              <div className="mr-2 w-1/5 bg-orange text-center">1/5</div>
+              <div className="w-3/5 bg-blue text-center">3/5</div>
+            </div>
+            <div className="flex mb-1 color-white">
+              <div className="mr-2 w-4/5 bg-blue text-center">4/5</div>
+              <div className="w-1/5 bg-orange text-center">1/5</div>
+            </div>
+            <div className="flex mb-1 color-white">
+              <div className="mr-2 w-1/5 bg-orange text-center">1/5</div>
+              <div className="w-4/5 bg-blue text-center">4/5</div>
+            </div>
+          </div>
+        </div>
+        <div className="px-2">
+          <div className="mb-1 bg-blue color-white text-center">Responsive</div>
+          <div className="flex mb-1 color-white">
+            <div className="mr-2 w-1/2 bg-blue text-center">1/2</div>
+            <div className="w-1/2 bg-blue text-center">1/2</div>
+          </div>
+          <div className="flex mb-1 color-white">
+            <div className="mr-2 w-1/3 bg-blue text-center">1/3</div>
+            <div className="mr-2 w-1/3 bg-blue text-center">1/3</div>
+            <div className="w-1/3 bg-blue text-center">1/3</div>
+          </div>
+          <div className="flex mb-1 color-white">
+            <div className="mr-2 w-2/3 bg-orange text-center">2/3</div>
+            <div className="w-1/3 bg-blue text-center">1/3</div>
+          </div>
+          <div className="flex mb-1 color-white">
+            <div className="mr-2 w-1/3 bg-blue text-center">1/3</div>
+            <div className="w-2/3 bg-orange text-center">2/3</div>
+          </div>
+          <div className="flex mb-1 color-white">
+            <div className="mr-2 w-1/4 bg-blue text-center">1/4</div>
+            <div className="mr-2 w-1/4 bg-blue text-center">1/4</div>
+            <div className="mr-2 w-1/4 bg-blue text-center">1/4</div>
+            <div className="w-1/4 bg-blue text-center">1/4</div>
+          </div>
+          <div className="flex mb-1 color-white">
+            <div className="mr-2 w-2/4 bg-orange text-center">2/4</div>
+            <div className="mr-2 w-1/4 bg-blue text-center">1/4</div>
+            <div className="w-1/4 bg-blue text-center">1/4</div>
+          </div>
+          <div className="flex mb-1 color-white">
+            <div className="mr-2 w-1/4 bg-blue text-center">1/4</div>
+            <div className="mr-2 w-2/4 bg-orange text-center">2/4</div>
+            <div className="w-1/4 bg-blue text-center">1/4</div>
+          </div>
+          <div className="flex mb-1 color-white">
+            <div className="mr-2 w-1/4 bg-blue text-center">1/4</div>
+            <div className="mr-2 w-1/4 bg-blue text-center">1/4</div>
+            <div className="w-2/4 bg-orange text-center">2/4</div>
+          </div>
+          <div className="flex mb-1 color-white">
+            <div className="mr-2 w-1/5 bg-blue text-center">1/5</div>
+            <div className="mr-2 w-1/5 bg-blue text-center">1/5</div>
+            <div className="mr-2 w-1/5 bg-blue text-center">1/5</div>
+            <div className="mr-2 w-1/5 bg-blue text-center">1/5</div>
+            <div className="w-1/5 bg-blue text-center">1/5</div>
+          </div>
+          <div className="flex mb-1 color-white">
+            <div className="mr-2 w-2/5 bg-orange text-center">2/5</div>
+            <div className="mr-2 w-1/5 bg-blue text-center">1/5</div>
+            <div className="mr-2 w-1/5 bg-blue text-center">1/5</div>
+            <div className="w-1/5 bg-blue text-center">1/5</div>
+          </div>
+          <div className="flex mb-1 color-white">
+            <div className="mr-2 w-1/5 bg-blue text-center">1/5</div>
+            <div className="mr-2 w-2/5 bg-orange text-center">2/5</div>
+            <div className="mr-2 w-1/5 bg-blue text-center">1/5</div>
+            <div className="w-1/5 bg-blue text-center">1/5</div>
+          </div>
+          <div className="flex mb-1 color-white">
+            <div className="mr-2 w-1/5 bg-blue text-center">1/5</div>
+            <div className="mr-2 w-1/5 bg-blue text-center">1/5</div>
+            <div className="mr-2 w-2/5 bg-orange text-center">2/5</div>
+            <div className="w-1/5 bg-blue text-center">1/5</div>
+          </div>
+          <div className="flex mb-1 color-white">
+            <div className="mr-2 w-1/5 bg-blue text-center">1/5</div>
+            <div className="mr-2 w-1/5 bg-blue text-center">1/5</div>
+            <div className="mr-2 w-1/5 bg-blue text-center">1/5</div>
+            <div className="w-2/5 bg-orange text-center">2/5</div>
+          </div>
+          <div className="flex mb-1 color-white">
+            <div className="mr-2 w-3/5 bg-orange text-center">3/5</div>
+            <div className="mr-2 w-1/5 bg-blue text-center">1/5</div>
+            <div className="w-1/5 bg-blue text-center">1/5</div>
+          </div>
+          <div className="flex mb-1 color-white">
+            <div className="mr-2 w-1/5 bg-blue text-center">1/5</div>
+            <div className="mr-2 w-3/5 bg-orange text-center">3/5</div>
+            <div className="w-1/5 bg-blue text-center">1/5</div>
+          </div>
+          <div className="flex mb-1 color-white">
+            <div className="mr-2 w-1/5 bg-blue text-center">1/5</div>
+            <div className="mr-2 w-1/5 bg-blue text-center">1/5</div>
+            <div className="w-3/5 bg-orange text-center">3/5</div>
+          </div>
+          <div className="flex mb-1 color-white">
+            <div className="mr-2 w-4/5 bg-orange text-center">5/5</div>
+            <div className="w-1/5 bg-blue text-center">1/5</div>
+          </div>
+          <div className="flex mb-1 color-white">
+            <div className="mr-2 w-1/5 bg-blue text-center">1/5</div>
+            <div className="w-4/5 bg-orange text-center">5/5</div>
+          </div>
+        </div>
       </div>
     );
   }
