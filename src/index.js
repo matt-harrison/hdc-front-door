f99268cd3c88c59a99449a710277b5ef8ba89596
Matt Harrison <matthewdh@gmail.com>
2022-12-15 15:43:59 -0500

    rebuilt from modern create-react-app.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-// import App from './GuttersDemo';
+// import App from './App';
+import App from './GuttersDemo';
 import './css/main.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
